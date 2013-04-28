d1829c3400577e63eb3f1b767a940f351dce981b
robertv <rvanderwall@yahoo.com>
2013-04-27 20:38:18 -0400

    commit changes to add tests

--- TraderApp/tests.py
+++ TraderApp/tests.py
@@ -5,12 +5,21 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+#from django.test import TestCase
+import unittest
+#from TraderApp.listing_processor import extract_nouns
 
-
-class SimpleTest(TestCase):
+class SimpleTest(unittest.TestCase):
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+#    def can_get_one_noun(self):
+#        nouns = extract_nouns("car")
+#        self.assertEqual(len(nouns), 1)
+#        self.assertEqual(nouns[0], "car")
+
+
+unittest.main()
